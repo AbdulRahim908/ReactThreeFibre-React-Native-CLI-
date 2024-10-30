1bf9f4a109771fcad2df7fe72f5d3b626bfba93c
AbdulRahim908 <89729367+AbdulRahim908@users.noreply.github.com>
2024-10-30 10:53:03 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import {View, Text, StyleSheet} from 'react-native';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber/native';
+import Model from './src/Components/Model';
+import useControls from 'r3f-native-orbitcontrols';
+const App = () => {
+  const [OrbitControls, events] = useControls();
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.modelContainer}{...events}>
+        <Canvas>
+          <OrbitControls enablePan={false}/>
+          <directionalLight position={[1,0,0]} args={['white',5]}/>
+          <directionalLight position={[-1,0,0]} args={['white',5]}/>
+          <directionalLight position={[0,1,0]} args={['white',5]}/>
+          <directionalLight position={[0,-1,0]} args={['white',5]}/>
+          <directionalLight position={[0,0,1]} args={['white',5]}/>
+          <directionalLight position={[0,0,-1]} args={['white',5]}/>
+          <Suspense fallback={null}>
+            <Model/>
+          </Suspense>
+        </Canvas>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.text}>Grey chair</Text>
+        <Text style={styles.text}>$180</Text>
+        <Text style={styles.text}>
+          Grey chain Grey chainGrey chainGrey chainGrey chainGrey chainGrey
+          chainGrey chainGrey chainGrey chainGrey chainGrey chain
+        </Text>
+      </View>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'F6F7FB',
+  },
+  modelContainer: {
+    flex: 2,
+  },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  text:{
+    color:'black',
+    fontSize:20,
+    marginHorizontal:20,
+    marginTop:10
+  }
+});
